dc1beb2d7cb19d7f67b6451f9cb5aa1782a73277
Yj <tiger8084@naver.com>
2022-11-11 11:46:21 +0900

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 # $1: 유튜브 URL
 # $2: 파일이름 + .mp4
 
-# cd /var/app/current/
 cd ~
 mkdir upload
 cd upload
-# FILE="~home/upload/yt-dlp"
-# if [ ! -e $FILE ];then
-#   echo "File not exists."
-# 	wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
-# fi
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+
+if [ ! -d "~/upload" ]; then
+  mkdir ~/upload
+fi
+
+FILE="~/upload/yt-dlp"
+if [ ! -e $FILE ]; then
+  echo "File not exists."
+  wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+fi
 chmod u+x yt-dlp # 실행 권한 문제
-# ./yt-dlp -f 140 https://www.youtube.com/watch?v=doYeFZ6saRY -o test.m4a
 ./yt-dlp -f 140 $1 -o $2
